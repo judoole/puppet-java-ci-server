712767a8b07411ce164fed54486b650e0fe9b86d
Ole Christian Langfjaeran <judoole@gmail.com>
2013-02-10 18:34:24 +0100

    Jenkins on board using tag 0.2.4 of rtyler/jenkins

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,13 +1,3 @@
-class base{
-  exec { 'apt-get update':
-    command => '/usr/bin/apt-get update'
-  }
-  package { jdk:
-    ensure => installed,
-    name => "openjdk-7-jdk",
-  }
-}
-
-include base
-#include jenkins
-include sonar
+#include base
+include jenkins
+#include sonar
